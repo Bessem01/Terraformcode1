8e10a60d2bb200aa50953d26916c1ad95f8d2e50
Bessem <bess4nolan@gmail.com>
2023-09-19 23:15:15 -0400

    added lightsail

---
+++ lightsail.tf
@@ -0,0 +1,16 @@
+resource "aws_lightsail_instance" "example" {
+  name              = "example-instance"
+  availability_zone = "us-east-1a" 
+  blueprint_id      = "amazon_linux_2" 
+  instance_type     = "nano_2_0" 
+  #key_pair_name     = "your-key-pair-name" 
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo yum install httpd -y
+              sudo systemctl start httpd
+              sudo systemctl enable httpd
+             echo "<h1>This Server is created using Terraform </h1>" | sudo tee /var/www/html/index.html
+              EOF
+}
+
